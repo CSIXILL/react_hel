db8c30b197a8676c65acd4b38b8d147c07f0f82f
Your Name <you@example.com>
2023-02-14 19:31:32 +0200

    Buttons

--- react-hel/src/App.js
+++ react-hel/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import './App.css';
 import { Message } from "./pages/Message";
+import { Buttons } from "./pages/Buttons";
+
 
 function App() {
   return (
     <div>
+      
       <Message />
+      <Buttons />
+      
     </div>
   );
 }

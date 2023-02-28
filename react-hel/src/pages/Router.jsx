6b9eac976c56d9f2da44be8d28118058751c3bd0
Your Name <you@example.com>
2023-02-28 19:28:50 +0200

    Router

---
+++ react-hel/src/pages/Router.jsx
@@ -0,0 +1,17 @@
+import { Outlet, Link } from "react-router-dom";
+import React from "react";
+
+export const Router = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+
+                </li>
+                <li>
+                    
+                </li>
+            </ul>
+        </div>
+    );
+}

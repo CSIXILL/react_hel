6f7831b7bc571ce2729bfc3cd0c78d240c1465c2
Your Name <you@example.com>
2023-03-07 19:29:45 +0200

    Router,App.js

--- react-hel/src/pages/Router.jsx
+++ react-hel/src/pages/Router.jsx
@@ -3,15 +3,18 @@ import React from "react";
 
 export const Router = () => {
     return (
+        <>
         <div>
             <ul>
                 <li>
-
+                    <Link to="/">Home </Link>
                 </li>
                 <li>
-                    
+                    <Link to="/buttons">Buttons</Link>
                 </li>
             </ul>
         </div>
+        <Outlet />
+        </>
     );
 }
